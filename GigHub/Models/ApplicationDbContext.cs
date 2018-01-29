87a5e9854758fae465a62e42d2d6db7bd0b0d14b
Keian.Barton@civica.co.uk <keian.barton@civica.co.uk>
2018-01-29 09:15:26 +0000

    Updated models/migration to include followings

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace GigHub.Models
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -22,9 +23,20 @@ namespace GigHub.Models
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             // Each attendance has a required gig, with many attendances to gigs
+            // Do not delete gigs when we delete an attendnace
             modelBuilder.Entity<Attendance>()
                 .HasRequired(a => a.Gig)
                 .WithMany()
+                .WillCascadeOnDelete(false);
+
+            // Do not delete followers/followees when we delete a user
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
+                .WillCascadeOnDelete(false);
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
                 .WillCascadeOnDelete(false);
 
             base.OnModelCreating(modelBuilder);
