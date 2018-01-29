a8da459d9571a3f7241aa68d762fe92b8aac727f
Keian.Barton@civica.co.uk <keian.barton@civica.co.uk>
2018-01-29 10:09:21 +0000

    Implemented "Following" API

---
+++ GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,41 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+            var followingAlreadyExists = _context.Followings
+                .Any(f => f.FollowerId == userId
+                        && f.FolloweeId == dto.FolloweeId);
+            if (followingAlreadyExists)
+                return BadRequest("The attendance already exists.");
+
+            var following = new Following
+            {
+                FolloweeId = userId,
+                FollowerId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
