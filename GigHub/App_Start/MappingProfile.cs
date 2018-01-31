4dfd62f46e4a4b906d686423d9f713919820993a
Keian.Barton@civica.co.uk <keian.barton@civica.co.uk>
2018-01-31 13:02:29 +0000

    Mapping domain objects to DTOs

---
+++ GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GigHub.Dtos;
+using GigHub.Models;
+using GigHub.Models.Notifications;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ApplicationUser, UserDto>();
+            CreateMap<Gig, GigDto>();
+            CreateMap<Notification, NotificationDto>();
+        }
+    }
+}
