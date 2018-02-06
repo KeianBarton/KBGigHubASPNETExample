d6f020080eee16bc342f82d4e010101850856f58
Keian.Barton@civica.co.uk <keian.barton@civica.co.uk>
2018-02-06 11:14:05 +0000

    Configured basic Ninject

--- GigHub/Global.asax.cs
+++ GigHub/Global.asax.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using GigHub.App_Start;
+using Ninject;
+using Ninject.Extensions.Conventions;
+using Ninject.Web.Common.WebHost;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -7,16 +10,35 @@ using System.Web.Routing;
 
 namespace GigHub
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : NinjectHttpApplication
     {
-        protected void Application_Start()
+        protected override void OnApplicationStarted()
         {
+            base.OnApplicationStarted();
+
             Mapper.Initialize(cfg => cfg.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            RegisterServices(kernel);
+            kernel.Bind(x =>
+            {
+                x.FromThisAssembly()
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+            return kernel;
+        }
+
+        private void RegisterServices(IKernel kernel)
+        {
         }
     }
 }
